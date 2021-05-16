c97097756f9fd7931574454e6267b1d205d152e9
haoxiaoli <haoxiaoli@baidu.com>
2021-05-16 20:35:26 +0800

    导航+轮播组件

---
+++ src/components/sideMenu/mockup.ts
@@ -0,0 +1,58 @@
+export const sideMenuList = [
+    {
+        title: '主题旅游',
+        subMenu: [
+            {
+                title: '爸妈游',
+                subMenu: ['祈福拜佛', '古镇游玩', '桂林山水', '红色之旅']
+            },
+            {
+                title: '亲子游',
+                subMenu: ['上海迪士尼', '探险体验', '海洋公园', '感受名校']
+            },
+            {
+                title: '蜜月游',
+                subMenu: ['浪漫海岛', '豪华酒店', '购物血拼', '全球婚礼']
+            }
+        ]
+    },
+    {
+        title: '周边游',
+        subMenu: [
+            {
+                title: '周边目的地',
+                subMenu: ['杭州', '南京', '苏州', '黄山']
+            },
+            {
+                title: '热门景点',
+                subMenu: ['普陀山', '千岛湖', '乌镇古镇', '宏村']
+            } 
+        ]
+    },
+    {
+        title: '国内游',
+        subMenu: [
+            {
+                title: '广东',
+                subMenu: []
+            },
+            {
+                title: 'asdf',
+                subMenu: ['asdfasd']
+            } 
+        ]
+    },
+    {
+        title: 'XXX',
+        subMenu: [
+            {
+                title: 'XXX',
+                subMenu: ['XX']
+            },
+            {
+                title: 'XXX',
+                subMenu: ['XXXX']
+            } 
+        ]
+    }
+]
